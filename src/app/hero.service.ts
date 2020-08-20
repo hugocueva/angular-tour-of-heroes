41be293b5a9108d821f7e50fd10fe55fdb26de5b
Hugo Cueva <hugo.cueva@owners-link.com>
2020-08-20 13:44:23 -0500

    More progress

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero'; 
+import { HEROES } from './mock-heroes'; 
+import { MessageService } from './message.service'; 
+import { Observable,of } from "rxjs";
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]>{
+    this.messageService.add('HeroService: fetched heroes'); 
+    return of(HEROES); 
+  }
+}
